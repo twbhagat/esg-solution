fe7d7ce3523525356c9594fa4199e82bd88f5c7b
Bhagat <twinkle.bhagat@capgemini.com>
2021-06-10 13:47:47 -0400

    components folder

---
+++ src/components/QuestionaireStart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class QuestionaireStart extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
