6a6c2ca7aa7cad72180a1f6f41934a0123780058
Bhagat <twinkle.bhagat@capgemini.com>
2021-06-11 15:10:53 -0400

    qstart progress

--- src/components/QuestionaireStart.js
+++ src/components/QuestionaireStart.js
@@ -4,7 +4,21 @@ export default class QuestionaireStart extends Component {
     render() {
         return (
             <div>
-                
+                <div className="row">
+                    <div className="col-sm-6">
+                            <h5>Image</h5>
+                    </div>
+
+                    <div className="col-sm-6"> 
+                        <div>
+                            <h5>We do not inherit the Earth from our Ancestors, we borrow it from our Children</h5>
+                            <p>Are you interested in applying Socially Responsible Investment (SRI) feature to your profile holdings to align your personal values around enviornmental, social and corporate governance with your investment choices?</p>
+                            <u>I want to know more about ESG and its benefits</u>
+                            <p>This is the best time to put value in your values</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         )
     }
