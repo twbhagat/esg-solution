7e845c11659fe87a66fe653f07ae2beff0d7630c
Bhagat <twinkle.bhagat@capgemini.com>
2021-06-11 17:10:57 -0400

    added strengths page + QuestionaireStart buttons

---
+++ src/components/StrengthsForm.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class StrengthsForm extends Component {
+    render() {
+        return (
+            <div>
+                
+                <div className="card card-success">
+                    <div className="card-header">
+                     <h2 className="card-title">Please indicate the strengths or levels of your interest in your selected enviornmental causes for investing through the sliders below:</h2>
+                    </div>
+                    <div className="card-body"></div>
+
+                    
+                {/* <div class="row">
+                    <div class="col-sm-6">
+
+                            
+                        <div class="slider-blue">
+                        <input type="text" value="" class="slider form-control" data-slider-min="-200" data-slider-max="200"
+                            data-slider-step="5" data-slider-value="[-100,100]" data-slider-orientation="horizontal"
+                            data-slider-selection="before" data-slider-tooltip="show"></input>
+                        </div>
+                            
+                    </div>
+                </div> */}
+               
+                    
+                
+                </div>
+            </div>
+        )
+    }
+}
